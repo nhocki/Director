2fb8cb50029be14d243664ea9269581386329aea
Nicolás Hock Isaza <nhocki@gmail.com>
2010-09-14 17:41:48 -0500

    Added suburi option

--- initializers/load_config.rb
+++ initializers/load_config.rb
@@ -6,6 +6,7 @@ configure :development do
   @my_domains      = @config["development"]["domains"]
   @admin_user      = @config["development"]["admin_username"]
   @admin_pwd       = @config["development"]["admin_password"]
+  @sub_uri         = @config["development"]["sub_uri"]
 end
 
 configure :production do
@@ -14,9 +15,11 @@ configure :production do
   @my_domains      = @config["production"]["domains"]
   @admin_user      = @config["production"]["admin_username"]
   @admin_pwd       = @config["production"]["admin_password"]
+  @sub_uri         = @config["production"]["sub_uri"]
 end
 
 set :redirect_url, @redirect_url
 set :domains, @my_domains
 set :admin_user, @admin_user
 set :admin_password, @admin_pwd
+set :sub_uri, @sub_uri
